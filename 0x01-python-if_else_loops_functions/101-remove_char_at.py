2f4d1f532d4c3920bdc76ff7099ea204bc0d6a77
Sheila Kahwai <sheilakahwai@users.noreply.github.com>
2022-08-23 13:02:36 +0000

    fix pycodestyle error

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-        str_cpy = ""
-        for idx in range(len(str)):
-            if idx != n:
-                str_cpy += str[idx]
-        return str_cpy
+    str_cpy = ""
+    for idx in range(len(str)):
+        if idx != n:
+            str_cpy += str[idx]
+    return str_cpy
