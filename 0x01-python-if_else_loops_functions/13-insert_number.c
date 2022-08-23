9c4a77111e5221c8c220e711fc9132b072c5f783
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-23 08:33:03 -0700

    adjust end of while loop

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -23,7 +23,7 @@ listint_t *insert_node(listint_t **head, int number)
     *head = new;
   else
     {
-      while (curr->n <= number)
+      while (curr->next->n < number)
 	curr = curr->next;
       new->next = curr->next;
       curr->next = new;
