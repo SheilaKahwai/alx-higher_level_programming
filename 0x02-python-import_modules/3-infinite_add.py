1a23c0adfcaccba89d9ab8b0c266f5464d206f80
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-25 02:04:37 -0700

    print sum of arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    res = 0
+    if num_args == 0:
+        print("0")
+    else:
+        for idx in range(1, num_args + 1):
+            res += int(sys.argv[idx])
+        print(res)
