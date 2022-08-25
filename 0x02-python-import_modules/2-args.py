c53322d7b32d0c28773b05553148a12a884eb89e
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-25 02:16:29 -0700

    fix string literals

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("0 arguments.")
+        print("{} arguments.".format(num_args))
     else:
         if num_args == 1:
-            print("1 argument:")
+            print("{} argument:".format(num_args))
         else:
             print("{} arguments:".format(num_args))
         for idx in range(1, num_args + 1):
