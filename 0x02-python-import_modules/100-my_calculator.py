508b53d017b656fea97e21d191fbd165b21f4a75
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-25 04:17:37 -0700

    fix bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,23 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-
     if num_args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+        
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    
+    if op is '+':
+        res = add(a, b)
+    elif op is '-':
+        res = sub(a, b)
+    elif op is '*':
+        res = mul(a, b)
+    elif op is '/':
+        res = div(a, b)
     else:
-        if op == '+':
-            res = add(a, b)
-        elif op == '-':
-            res = sub(a, b)
-        elif op == '*':
-            res = mul(a, b)
-        elif op == '/':
-            res = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        print("{} {} {} = {}".format(a, op, b, res))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{} {} {} = {}".format(a, op, b, res))
