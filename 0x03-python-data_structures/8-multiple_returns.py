774d6ad19b53641e29cbc00809ab8a2fba4064cf
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-26 05:16:14 -0700

    add condition for when sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,9 @@
 
 def multiple_returns(sentence):
     str_len = len(sentence)
-    first_char = sentence[:1]
+    if str_len == 0:
+        first_char = None
+    else:
+        first_char = sentence[:1]
     tup_c = (str_len, first_char)
     return tup_c
