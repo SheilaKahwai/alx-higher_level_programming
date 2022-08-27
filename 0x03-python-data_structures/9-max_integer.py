4321362869af63d7d7f7b4df66e46fce8697ee64
Sheila Kahwai <sheilakahwai@users.noreply.github.com>
2022-08-27 11:25:05 +0000

    assign max_num to first integer of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 
 def max_integer(my_list=[]):
     if my_list is not None:
-        max_num = 0
         if len(my_list) == 0:
             return None
+        max_num = my_list[0]
         for num in my_list:
             if num > max_num:
                 max_num = num
