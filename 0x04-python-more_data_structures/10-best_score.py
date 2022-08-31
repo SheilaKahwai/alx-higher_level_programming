0afa0f47934ed3ad99d0c59752e8b5c412311c30
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-08-31 05:57:25 -0700

    add condition if dictionary is empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return
     best_key = []
     best_key_str = ""
