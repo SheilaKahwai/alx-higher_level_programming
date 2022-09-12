eef39015d9a0d6919713fb71b097aa10152094fc
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-12 02:20:17 -0700

    add count variable in case argument is 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print()
-        return i + 1
-    except IndexError:
-        print()
-        return i
+            count += 1
+        except IndexError:
+            pass
+    print()
+    return count
