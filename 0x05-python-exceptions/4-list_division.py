1cf2254d29c9fe9417c97c6109a894cfe8b19210
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-12 02:52:35 -0700

    fix exception message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,11 +11,10 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except ZeroDivisionError:
             res = 0
-            print("division by zero")
+            print("division by 0")
         except IndexError:
             res = 0
             print("out of range")
         finally:
-            pass
-        new_list.append(res)
+            new_list.append(res)
     return new_list
