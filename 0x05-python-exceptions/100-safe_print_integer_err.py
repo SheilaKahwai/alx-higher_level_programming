aa1ed11bca93588af393d0ba129a0d0151d02f10
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-12 03:39:09 -0700

    fix bug

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as error:
-        print("Exception: {}".format(error))
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
