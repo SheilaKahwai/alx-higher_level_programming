89ff009480b5308aec863716655ebe460c0b33ee
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-12 08:54:25 -0700

    tasks 0 to 6

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size
+    Instantiation with optional size.
+    Public instance method: def area(self).
+    """
+    
+    def __init__(self, size=0):
+        """Initializes the object's data."""
+        if type(size)is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+    def area(self):
+        """Returns a square's area."""
+        sq_area = self.__size ** 2
+        return sq_area
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size:
+        - property def size(self)
+        - property setter def size(self, value)
+    Private instance attribute: position:
+        - property def position(self)
+        - property setter def position(self, value)
+    Instantiation with optional size and optional position.
+    Public instance method: def area(self).
+    Public instance method: def my_print(self).
+    """
+    
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the object's data."""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value > 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    def area(self):
+        """Returns the current square area."""
+        sq_area = self.__size ** 2
+        return sq_area
+
+    def my_print(self):
+        """Prints to stdout the square with the character #,
+        at the position given by the position attribute.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position to a value."""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
