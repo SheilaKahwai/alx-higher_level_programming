7b3e0264757eacf30590c641f5b04d5b7af2e9b9
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-12 03:34:38 -0700

    add exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error:
+    except (ValueError, TypeError) as error:
         print("Exception: {}".format(error))
         return False
