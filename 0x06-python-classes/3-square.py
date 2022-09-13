2bc7bf84a6ada1a11ba5eefd7d677cf0364b86f7
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-13 09:27:31 -0700

    add method description

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""A class Square that defines a square."""
+
 
 class Square:
     """Represents a square.
@@ -9,7 +11,7 @@ class Square:
     
     def __init__(self, size=0):
         """Initializes the object's data."""
-        if type(size)is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
