e15d13a9cdecae5c7daa93f8b06a03598b72bd3c
SheilaKahwai <104775232+SheilaKahwai@users.noreply.github.com>
2022-09-13 20:01:13 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,6 +34,22 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
         
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position to a value."""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+        
     def area(self):
         """Returns the current square area."""
         sq_area = self.__size ** 2
@@ -54,19 +70,3 @@ class Square:
                 for j in range(self.__size):
                     print("#", end="")
                 print()
-
-    @property
-    def position(self):
-        """Retrieves the position."""
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """Sets the position to a value."""
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
