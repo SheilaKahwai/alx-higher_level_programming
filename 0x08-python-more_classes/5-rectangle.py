61ffea9aacc4f7f10ed073ee97693d20b7b0e57c
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-19 04:09:21 -0700

    delete instance of a rectangle.

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -34,6 +34,10 @@ class Rectangle:
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
+    def __del__(self):
+        """Deletes an instance of a rectangle."""
+        print("Bye Rectangle...")
+
     @property
     def width(self):
         """Retrieves the width."""
