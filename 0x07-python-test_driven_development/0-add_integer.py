47d2734add48577bb5170d6d79dac1d3ac9f43c0
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-19 09:19:15 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ Module add_integer
 Adds two integers.
 """
 
-def add_integer, b=98):
+def add_integer(a, b=98):
     """Adds two integers."""
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
