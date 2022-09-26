90b39cffdef2e57bc3415c1a57641def5103bff1
SheilaKahwai <104775232+SheilaKahwai@users.noreply.github.com>
2022-09-26 13:56:11 +0300

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,6 +7,12 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area():
         """Finds the area of a rectangle."""
