3f9d4fd06230e33c7e1f5a88044e6e135eab0fd5
Sheila Kahwai <sheilakahwai@users.noreply.github.com>
2022-09-27 10:31:58 +0000

    task 0 to 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ and then saves them to a file.
 
 
 import sys
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item(args, filename):
@@ -17,12 +17,12 @@ def add_item(args, filename):
         -filename: name of json file
     """
     try:
-        f = load(filename)
-    except:
+        f = load_from_json(filename)
+    except Exception:
         f = []
     for s in args:
         f.append(s)
-    save(f, filename)
+    save_to_json(f, filename)
 
 
 if __name__ == "__main__":
