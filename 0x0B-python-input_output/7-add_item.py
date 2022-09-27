a1b3a543a67b8096ab384ec1fa794f9ee333e962
Sheila Kahwai <sheilakahwai@users.noreply.github.com>
2022-09-27 10:26:27 +0000

    task 0 to 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
 """Module 7-add_item.
 A script that adds all arguments to a Python list,
 and then saves them to a file.
 """
 
 
+import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
