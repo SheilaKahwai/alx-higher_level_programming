596a06cefcf2ec0742dcf1b04b16d25a2ecb7156
SheilaKahwai <kahwaisheila@gmail.com>
2022-10-01 09:52:47 -0700

    add test cases for Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -25,6 +25,10 @@ class TestBase(unittest.TestCase):
         print("Base setUp")
 
     def tearDown(self):
+        del self.obj1
+        del self.obj2
+        del self.obj3
+        del self.obj4
         print("Base tearDown")
 
     def test_instance_id(self):
@@ -32,3 +36,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(self.obj2.id, 2)
         self.assertEqual(self.obj3.id, 9)
         self.assertEqual(self.obj4.id, 3)
+        self.assertEqual(Base("Betty").id, "Betty")
+        self.assertEqual(Base([1, 2, 3]).id, [1, 2, 3])
+        self.assertEqual(Base({"id": 89}).id, {"id": 89})
+        self.assertEqual(Base((7,)).id, (7,))
